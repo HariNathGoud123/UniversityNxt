aea6c1a58e84101a0ffca4166337179f680ed222
HariNathGoud123 <146013645+HariNathGoud123@users.noreply.github.com>
2024-10-16 11:38:36 +0530

    Update ProfessorController.java

--- src/main/java/com/example/university/controller/ProfessorController.java
+++ src/main/java/com/example/university/controller/ProfessorController.java
@@ -1,53 +1,56 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.web.bind.annotation.*;
- * import java.util.ArrayList;
- * 
- */
-
 package com.example.university.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.university.model.Course;
+import com.example.university.model.Professor;
+import com.example.university.service.ProfessorJpaService;
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
-import com.example.university.service.*;
-import com.example.university.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
 
 @RestController
 public class ProfessorController {
-  @Autowired
-  ProfessorJpaService professorJpaService;
 
-  @GetMapping("/professors")
-  public ArrayList<Professor> getProfessors() {
-    return professorJpaService.getProfessors();
-  }
 
-  @GetMapping("/professors/{professorId}")
-  public Professor getProfessorById(@PathVariable("professorId") int professorId) {
-    return professorJpaService.getProfessorById(professorId);
-  }
+    @Autowired
+    private ProfessorJpaService professorJpaService;
 
-  @PostMapping("/professors")
-  public Professor addProfessor(@RequestBody Professor professor) {
-    return professorJpaService.addProfessor(professor);
-  }
 
-  @PutMapping("/professors/{professorId}")
-  public Professor updateProfessor(@RequestBody Professor professor, @PathVariable("professorId") int professorId) {
-    return professorJpaService.updateProfessor(professor, professorId);
-  }
+    @GetMapping("/professors")
+    public List<Professor> getProfessors(){
+        return professorJpaService.getProfessors();
+    }
 
-  @DeleteMapping("/professors/{professorId}")
-  public void deleteProfessorById(@PathVariable("professorId") int professorId) {
-    professorJpaService.deleteProfessorById(professorId);
-  }
 
-  @GetMapping("professors/{professorId}/courses")
-  public List<Course> getProfessorCourses(@PathVariable("professorId") int professorId) {
-    return professorJpaService.getProfessorCourses(professorId);
-  }
+    @GetMapping("/professors/{professorId}")
+    public Professor getProfessorById(@PathVariable Integer professorId){
+        return professorJpaService.getProfessorById(professorId);
+    }
+
+
+    @PostMapping("/professors")
+    public Professor addProfessor(@RequestBody Professor professor){
+        return professorJpaService.addProfessor(professor);
+    }
+
+
+    @PutMapping("/professors/{professorId}")
+    public Professor updateProfessor(@RequestBody Professor professor, @PathVariable("professorId") int professorId) {
+        return professorJpaService.updateProfessor(professorId, professor);
+    }
+
+
+    @DeleteMapping("/professors/{professorId}")
+    public void deleteProfessor(@PathVariable("professorId") int professorId) {
+        professorJpaService.deleteProfessor(professorId);
+    }
+
+
+    @GetMapping("/professors/{professorId}/courses")
+    public List<Course> getProfessorCourses(@PathVariable("professorId") int professorId){
+        return professorJpaService.getProfessorCourses(professorId);
+    }
 }

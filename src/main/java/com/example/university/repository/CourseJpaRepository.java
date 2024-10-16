6864ebdc4bd75288ac2ff7ef3f3388c1696134c4
HariNathGoud123 <146013645+HariNathGoud123@users.noreply.github.com>
2024-10-16 11:42:14 +0530

    Update CourseJpaRepository.java

--- src/main/java/com/example/university/repository/CourseJpaRepository.java
+++ src/main/java/com/example/university/repository/CourseJpaRepository.java
@@ -1,17 +1,10 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.data.jpa.repository.JpaRepository;
- * import org.springframework.stereotype.Repository;
- * 
- */
-
 package com.example.university.repository;
 
-import com.example.university.model.*;
+import com.example.university.model.Course;
+import com.example.university.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository

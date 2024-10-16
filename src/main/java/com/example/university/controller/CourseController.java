2984fc551db5dded0476985be6699ddd0061ebd0
HariNathGoud123 <146013645+HariNathGoud123@users.noreply.github.com>
2024-10-16 11:38:06 +0530

    Update CourseController.java

--- src/main/java/com/example/university/controller/CourseController.java
+++ src/main/java/com/example/university/controller/CourseController.java
@@ -1,25 +1,19 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.web.bind.annotation.*;
- * import java.util.ArrayList;
- * 
- */
-
 package com.example.university.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.university.model.Course;
+import com.example.university.model.Professor;
+import com.example.university.model.Student;
+import com.example.university.service.CourseJpaService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.*;
-import com.example.university.service.*;
-import com.example.university.model.*;
 
 @RestController
 public class CourseController {
+
     @Autowired
-    CourseJpaService courseJpaService;
+    public CourseJpaService courseJpaService;
 
     @GetMapping("/courses")
     public List<Course> getCourses() {
@@ -33,27 +27,28 @@ public class CourseController {
 
     @PostMapping("/courses")
     public Course addCourse(@RequestBody Course course) {
+
         return courseJpaService.addCourse(course);
     }
 
-    @PutMapping("courses/{courseId}")
-    public Course updatecourse(@RequestBody Course course, @PathVariable("courseId") int courseId) {
-        return courseJpaService.updateCourse(course, courseId);
+    @PutMapping("/courses/{courseId}")
+    public Course updateCourse(@PathVariable("courseId") int courseId, @RequestBody Course course) {
+        return courseJpaService.updateCourse(courseId, course);
     }
 
-    @DeleteMapping("courses/{courseId}")
-    public void deleteCourseById(@PathVariable("courseId") int courseId) {
-        courseJpaService.deleteCourseById(courseId);
+    @DeleteMapping("/courses/{courseId}")
+    public void deleteCourse(@PathVariable("courseId") int courseId) {
+        courseJpaService.deleteCourse(courseId);
     }
 
     @GetMapping("/courses/{courseId}/professor")
-    public Professor getCourserProfessor(@PathVariable("courseId") int courseId) {
+    public Professor getCourseProfessor(@PathVariable("courseId") int courseId) {
         return courseJpaService.getCourseProfessor(courseId);
     }
 
     @GetMapping("/courses/{courseId}/students")
-    public List<Student> ListCourseStudents(@PathVariable("courseId") int courseId) {
-        return courseJpaService.getCourseStudentsById(courseId);
+    public List<Student> getCourseStudents(@PathVariable("courseId") int courseId) {
+        return courseJpaService.getCourseStudents(courseId);
     }
 
 }

a0dc0f0dcd4c87443a95f0624367828994ca9c8c
HariNathGoud123 <146013645+HariNathGoud123@users.noreply.github.com>
2024-10-16 11:40:49 +0530

    Update Professor.java

--- src/main/java/com/example/university/model/Professor.java
+++ src/main/java/com/example/university/model/Professor.java
@@ -1,58 +1,45 @@
-/*
- *
- * You can use the following import statements
- * 
- * import javax.persistence.*;
- * 
- */
-
 package com.example.university.model;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "professor")
 public class Professor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int professorId;
+
     @Column(name = "name")
     private String professorName;
+
     @Column(name = "department")
     private String department;
+
+    public Professor() {
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
 
     public void setProfessorId(int professorId) {
         this.professorId = professorId;
     }
 
-    public int getProfessorId() {
-        return this.professorId;
+    public String getProfessorName() {
+        return professorName;
     }
 
     public void setProfessorName(String professorName) {
         this.professorName = professorName;
     }
 
-    public String getProfessorName() {
-        return this.professorName;
-    }
-
-    public void setDepartMent(String department) {
-        this.department = department;
-    }
-
     public String getDepartment() {
-
-        return this.department;
+        return department;
     }
 
-    public Professor() {
-    }
-
-    public Professor(int professorId, String professorName, String department) {
-        this.professorId = professorId;
-        this.professorName = professorName;
+    public void setDepartment(String department) {
         this.department = department;
     }
 }
